67121f7ac5a332925791161a7dd8de970d3f666d
anacarolinamoreno <carolmoreno@gmail.com>
2021-04-27 22:15:00 -0300

    Arrumando pequenos erros

--- R/sivep_2_analise-covid-SP.R
+++ R/sivep_2_analise-covid-SP.R
@@ -20,6 +20,8 @@ library(lme4)
 # 1.7 - Total de óbitos por Covid por mês para cada faixa etária no ESP
 
 
+# Criar condições para delimitar o período dos casos entre o início de fevereiro e a segunda-feira da extração da base
+
 if(wday(today()) == 1) {
   ultima_segunda <- today() - 6
 } else {
@@ -27,8 +29,6 @@ if(wday(today()) == 1) {
 }
 
 inicio_pandemia <- ymd('2020-02-01')
-
-
 
 
 
@@ -72,27 +72,12 @@ covid_sp_dia <- left_join(x = internacoes_sp_dia, y = obitos_sp_dia, by = 'data'
   select(data, internacoes, mortes) %>%
   replace(is.na(.), 0) %>%
   pivot_longer(cols = c(internacoes, mortes), names_to = "categoria", values_to = "valor") %>%
-  write_rds("data/covid_sp_dia.rds") %>%
-  ggplot(aes(x=data, y=valor, group=categoria)) +
-  geom_line()+
-  labs(y = 'Total de internações por dia',
-       x = 'Fonte: Censo Covid/SES-SP')+
-  ggtitle("Novas internações e novas mortes confirmadas por Covid-19 (Estado de SP)")+
-  theme(
-    plot.background = element_rect(fill = "white"),
-    panel.background = element_rect(fill = "white", colour="grey"),
-    panel.grid.major = element_line(colour = "grey", linetype = "dotted"),
-    panel.grid.minor = element_line(colour = "grey", linetype = "dotted"),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    panel.grid.minor.y = element_blank(),
-  )+
-  geom_vline(xintercept=ymd("2021-03-24"), color="red", size=0.2)
+  write_rds("data/covid_sp_dia.rds")
 
 ggplot_covid_sp_dia <- read_rds("data/covid_sp_dia.rds") %>%
   ggplot(aes(x=data, y=valor, group=categoria, colour=categoria)) +
   geom_line()+
-  labs(y = 'Total de internações por dia',
+  labs(y = 'Total de internações e mortes por dia',
        x = 'Fonte: Censo Covid/SES-SP')+
   ggtitle("Novas internações e novas mortes confirmadas por Covid-19 (Estado de SP)")+
   theme(
@@ -106,34 +91,21 @@ ggplot_covid_sp_dia <- read_rds("data/covid_sp_dia.rds") %>%
   )+
   geom_vline(xintercept=ymd("2021-03-24"), color="blue", size=0.2)
 
-+
-  geom_label(data=annotation, aes(x=x, y=y, label=label),
-             color="orange",
-             size=7 , angle=45, fontface="bold")
-
 
 ggplot_covid_sp_dia
 
 
-+
-  geom_label(data=annotation, aes(x=x, y=y, label=label),
-             color="orange",
-             size=7 , angle=45, fontface="bold" )
-
-covid_sp_dia
-
-annotation <- data.frame(
-  x = ymd("2021-03-20"),
-  y = 2300,
-  label = "atraso"
-)
-
 
 # 1.2- Média móvel de internações e mortes por Covid por dia no ESP
 
-internacoes_sp_dia_media <- sivep %>%
-  filter(SG_UF == "SP" & diagnostico == "Covid-19" & !is.na(DT_INTERNA)) %>%
-  filter(DT_INTERNA > ymd('2020-02-29') & DT_INTERNA <= today()) %>%
+internacoes_sp_dia_media <- sivep_sp %>%
+  filter(
+    DT_INTERNA > inicio_pandemia
+    & DT_INTERNA < ultima_segunda
+    & SG_UF == "SP"
+    & diagnostico == "Covid-19"
+    & !is.na(DT_INTERNA)
+    & DT_INTERNA > "2020-01-31") %>%
   group_by(diagnostico, DT_INTERNA) %>%
   summarise(
     contagem = sum(contagem)
@@ -142,14 +114,18 @@ internacoes_sp_dia_media <- sivep %>%
     media_movel = round(rollapply(contagem, 7, mean, align = 'right', fill = NA), 0)
   ) %>%
   rename(
-    internacoes = media_movel,
+    internacoes_mm7d = media_movel,
     data = DT_INTERNA
   ) %>%
   replace(is.na(.), 0) %>%
-  select(diagnostico, data, internacoes)
+  select(diagnostico, data, internacoes_mm7d)
 
-obitos_sp_dia_media <- sivep %>%
-  filter(SG_UF == "SP" & diagnostico == "Covid-19" & !is.na(DT_EVOLUCA) & EVOLUCAO == 2) %>%
+obitos_sp_dia_media <- sivep_sp %>%
+  filter(SG_UF == "SP"
+         & diagnostico == "Covid-19"
+         & !is.na(DT_EVOLUCA)
+         & EVOLUCAO == 2
+         & DT_EVOLUCA <= ultima_segunda) %>%
   group_by(diagnostico, DT_EVOLUCA) %>%
   summarise(
     contagem = sum(contagem)
@@ -158,20 +134,117 @@ obitos_sp_dia_media <- sivep %>%
     media_movel = round(rollapply(contagem, 7, mean, align = 'right', fill = NA), 0)
   ) %>%
   rename(
-    mortes = media_movel,
+    mortes_mm7d = media_movel,
     data = DT_EVOLUCA
   ) %>%
   replace(is.na(.), 0) %>%
-  select(diagnostico, data, mortes)
+  select(diagnostico, data, mortes_mm7d)
 
 covid_sp_dia_media <- left_join(x = internacoes_sp_dia_media, y = obitos_sp_dia_media, by = 'data') %>%
   filter(!is.na(data)) %>%
-  select(data, internacoes, mortes) %>%
+  select(data, internacoes_mm7d, mortes_mm7d) %>%
   replace(is.na(.), 0) %>%
-  pivot_longer(cols = c(internacoes, mortes), names_to = "categoria", values_to = "valor") %>%
-  ggplot(aes(x=data, y=valor, group=categoria)) +
+  pivot_longer(cols = c(internacoes_mm7d, mortes_mm7d), names_to = "categoria", values_to = "valor") %>%
+  write_rds("data/covid_sp_dia_media.rds")
+
+ggplot_covid_sp_dia_media <- read_rds("data/covid_sp_dia_media.rds") %>%
+  ggplot(aes(x=data, y=valor, group=categoria, colour=categoria)) +
   geom_line()+
-  geom_point()
+  labs(y = 'Média móvel de internações e mortes por dia',
+       x = 'Fonte: Censo Covid/SES-SP')+
+  ggtitle("Novas internações e novas mortes confirmadas por Covid-19 (Estado de SP)")+
+  theme(
+    plot.background = element_rect(fill = "white"),
+    panel.background = element_rect(fill = "white", colour="grey"),
+    panel.grid.major = element_line(colour = "grey", linetype = "dotted"),
+    panel.grid.minor = element_line(colour = "grey", linetype = "dotted"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.minor.y = element_blank(),
+  )+
+  geom_vline(xintercept=ymd("2021-03-24"), color="blue", size=0.2)
 
 
-covid_sp_dia_media
+ggplot_covid_sp_dia_media
+
+
+# 1.3- Total de internações por Covid por semana epidemiológica no ESP
+
+internacoes_sp_semana <- sivep_sp %>%
+  filter(
+    DT_INTERNA > inicio_pandemia
+    & DT_INTERNA < ultima_segunda
+    & SG_UF == "SP"
+    & diagnostico == "Covid-19"
+    & !is.na(DT_INTERNA)
+    & DT_INTERNA > "2020-01-31") %>%
+  group_by(diagnostico, sem_interna) %>%
+  summarise(
+    contagem = sum(contagem)
+  ) %>%
+  rename(
+    internacoes = contagem,
+    semana = sem_interna
+  ) %>%
+  select(diagnostico, semana, internacoes) %>%
+  write_rds("data/covid_sp_semana.rds")
+
+ggplot_covid_sp_semana <- read_rds("data/covid_sp_semana.rds") %>%
+  ggplot(aes(x=semana, y=internacoes)) +
+  geom_line()+
+  labs(y = 'Total de internações por semana',
+       x = 'Fonte: Censo Covid/SES-SP')+
+  ggtitle("Internações confirmadas por Covid-19 (Estado de SP)")+
+  geom_vline(xintercept=ymd("2021-03-24"), color="blue", size=0.2)
+
+
+ggplot_covid_sp_semana
+
+
+# 1.4- Total de internações por Covid por mês no ESP
+
+internacoes_sp_mes <- sivep_sp %>%
+  filter(
+    DT_INTERNA > inicio_pandemia
+    & DT_INTERNA < ultima_segunda
+    & SG_UF == "SP"
+    & diagnostico == "Covid-19"
+    & !is.na(DT_INTERNA)
+    & DT_INTERNA > "2020-02-29") %>%
+  group_by(diagnostico, mes_ano_internacao1) %>%
+  summarise(
+    contagem = sum(contagem)
+  ) %>%
+  rename(
+    internacoes = contagem,
+    mes = mes_ano_internacao1
+  ) %>%
+  mutate(
+    mes = as.yearmon(mes, "%m/%Y")
+  ) %>%
+  select(diagnostico, mes, internacoes) %>%
+  arrange(mes) %>%
+  write_rds("data/covid_sp_mes.rds")
+
+ggplot_covid_sp_mes <- read_rds("data/covid_sp_mes.rds") %>%
+  ggplot(aes(x=mes, y=internacoes, colour="blue")) +
+  geom_bar(stat = "identity")+
+  geom_text(aes(x = mes,
+                y = internacoes,
+                vjust = -0.1,
+                size = 5,
+                label = internacoes,
+                colour = "red"))+
+  labs(y = 'Total de internações por mês',
+       x = 'Fonte: Censo Covid/SES-SP')+
+  ggtitle("Internações confirmadas por Covid-19 (Estado de SP)")
+
+ggplot_covid_sp_mes
+
+
+# 1.5 - Total de internações por Covid por mês para cada faixa etária no ESP
+
+
+
+# 1.6 - Total de internações em UTI por Covid por mês para cada faixa etária no ESP
+# 1.7 - Total de óbitos por Covid por mês para cada faixa etária no ESP
--- README.md
+++ README.md
@@ -5,8 +5,6 @@ Ana Carolina Moreno
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# Análises: Covid-19 no Estado de São Paulo
-
 ### Dados atualizados até **19/04/2021**
 
 <!-- badges: start -->
@@ -15,77 +13,42 @@ Ana Carolina Moreno
 Essa página contém análises de dados de casos confirmados de Covid-19 no
 Estado de São Paulo.
 
+## 1- **Total** de internações e mortes por dia
+
+-   Linha vermelha: internações
+-   Linha verde: mortes
+-   Entre meados de março e o início de abril o Estado de SP registrou
+    pelo menos 1.500 novos pacientes internados por dia
+-   Já as mortes ficaram pelo menos acima de 500 por dia no período
+-   O gráfico dá indícios de que existe uma relação entre o aumento da
+    pressão no sistema hospitalar provocou também um aumento do risco de
+    morrer pela doença
+
+(A linha azul indica a partir de quando os dados caem devido ao atraso
+de notificação)
+
 ``` r
-summary(cars)
-#>      speed           dist       
-#>  Min.   : 4.0   Min.   :  2.00  
-#>  1st Qu.:12.0   1st Qu.: 26.00  
-#>  Median :15.0   Median : 36.00  
-#>  Mean   :15.4   Mean   : 42.98  
-#>  3rd Qu.:19.0   3rd Qu.: 56.00  
-#>  Max.   :25.0   Max.   :120.00
+
+plot(ggplot_covid_sp_dia)
 ```
 
-You’ll still need to render `README.Rmd` regularly, to keep `README.md`
-up-to-date. `devtools::build_readme()` is handy for this. You could also
-use GitHub Actions to re-render `README.Rmd` every time you push. An
-example workflow can be found here:
-<https://github.com/r-lib/actions/tree/master/examples>.
+![](README_files/figure-gfm/ggplot_covid_sp_dia2-1.png)<!-- -->
 
-You can also embed plots, for example:
+-   2- ***Média móvel*** de internações e mortes por dia
 
-![](README_files/figure-gfm/pressure-1.png)<!-- -->
+-   Linha vermelha: internações
 
-Teste da Carol
+-   Linha verde: mortes
 
-    #> -- Attaching packages --------------------------------------- tidyverse 1.3.0 --
-    #> v ggplot2 3.3.3     v purrr   0.3.4
-    #> v tibble  3.1.0     v dplyr   1.0.5
-    #> v tidyr   1.1.3     v stringr 1.4.0
-    #> v readr   1.4.0     v forcats 0.5.1
-    #> -- Conflicts ------------------------------------------ tidyverse_conflicts() --
-    #> x dplyr::filter() masks stats::filter()
-    #> x dplyr::lag()    masks stats::lag()
-    #> 
-    #> Attaching package: 'lubridate'
-    #> The following objects are masked from 'package:base':
-    #> 
-    #>     date, intersect, setdiff, union
-    #> 
-    #> Attaching package: 'zoo'
-    #> The following objects are masked from 'package:base':
-    #> 
-    #>     as.Date, as.Date.numeric
-    #> 
-    #> Attaching package: 'tidylog'
-    #> The following objects are masked from 'package:dplyr':
-    #> 
-    #>     add_count, add_tally, anti_join, count, distinct, distinct_all,
-    #>     distinct_at, distinct_if, filter, filter_all, filter_at, filter_if,
-    #>     full_join, group_by, group_by_all, group_by_at, group_by_if,
-    #>     inner_join, left_join, mutate, mutate_all, mutate_at, mutate_if,
-    #>     relocate, rename, rename_all, rename_at, rename_if, rename_with,
-    #>     right_join, sample_frac, sample_n, select, select_all, select_at,
-    #>     select_if, semi_join, slice, slice_head, slice_max, slice_min,
-    #>     slice_sample, slice_tail, summarise, summarise_all, summarise_at,
-    #>     summarise_if, summarize, summarize_all, summarize_at, summarize_if,
-    #>     tally, top_frac, top_n, transmute, transmute_all, transmute_at,
-    #>     transmute_if, ungroup
-    #> The following objects are masked from 'package:tidyr':
-    #> 
-    #>     drop_na, fill, gather, pivot_longer, pivot_wider, replace_na,
-    #>     spread, uncount
-    #> The following object is masked from 'package:stats':
-    #> 
-    #>     filter
-    #> Loading required package: Matrix
-    #> 
-    #> Attaching package: 'Matrix'
-    #> The following objects are masked from 'package:tidyr':
-    #> 
-    #>     expand, pack, unpack
+-   O gráfico é o mesmo acima, mas está mais suavizado porque representa
+    a média móvel de 7 dias
 
-![](README_files/figure-gfm/unnamed-chunk-2-1.png)<!-- -->
+(A linha azul indica a partir de quando os dados caem devido ao atraso
+de notificação)
 
-In that case, don’t forget to commit and push the resulting figure
-files, so they display on GitHub.
+``` r
+
+plot(ggplot_covid_sp_dia_media)
+```
+
+![](README_files/figure-gfm/ggplot_covid_sp_dia_media2-1.png)<!-- -->
